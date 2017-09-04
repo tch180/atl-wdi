593f566e2b8e15e1d7fbcaf6574e19c27bcd4ea4
tch180 <--global>
2017-09-04 15:13:54 -0400

    homework done npm test not working in vscode, error code. npm WARN js-functions-hw-drilling@1.0.0 No repository field. will touch base with ben or danny on solution

--- homework/javaScript/function-drills/hw.js
+++ homework/javaScript/function-drills/hw.js
@@ -90,7 +90,21 @@ var anyStartingWithB = function(words){
 //    Assume that vowels are 'a', 'e', 'i', 'o', and 'u' (NOT 'y')
 // Edge Case: If `n` is less than zero, return `null`.
 var hasAtLeastNVowels = function(word, n){
-  // Your Code Here
+ if (n < 0); i < word.length; i++) {
+   return null;
+ }
+ var count = 0;
+ for (var i = 0; i <word.length; i++){
+   if (word.charAt(i) === 'a' || word.charAt(i) === 'A' ||
+      word.charAt(i) === 'e' || word.charAt(i) === 'E' ||
+    word.charAt(i) === 'i' || word.charAt(i) === 'I' ||
+  word.charAt(i) === 'o' || word.charAt(i) === 'O' ||
+ word.charAt(i) === 'u' || word.charAt(i) === 'U'
+) {
+  count++;
+}
+ }
+ return count >= n;
 };
 
 // #8
@@ -98,7 +112,13 @@ var hasAtLeastNVowels = function(word, n){
 // Output: an array of words from the original array that have at least two
 //          vowels
 var wordsWithAtLeastTwoVowels = function(words){
-  // Your Code Here
+  var results = [];
+  for (var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      results.push(words[i]);
+    }
+  }
+  return results;
 };
 
 // #9
@@ -106,7 +126,12 @@ var wordsWithAtLeastTwoVowels = function(words){
 // Output: `true` if ALL words have two or more vowels, `false` otherwise
 // Edge Case: If the array is empty, the function should return `true`.
 var allHaveAtLeastTwoVowels = function(words){
-  // Your Code Here
+  for (var i = 0; i < words.length; i++) {
+    if (!hasAtLeastNVowels(words[i], 2)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // #10
@@ -114,8 +139,13 @@ var allHaveAtLeastTwoVowels = function(words){
 // Output: `true` if there are ANY words have two or more vowels,
 //          `false` otherwise.
 var anyHaveAtLeastTwoVowels = function(words){
-  // Your Code Here
-};
+  for (var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      return true;
+    }
+  }
+  return false;
+};  
 
 // #11
 // Input: an array of words
@@ -123,7 +153,12 @@ var anyHaveAtLeastTwoVowels = function(words){
 //          `false` otherwise
 // Edge Case: If the array is empty, the function should return `true`.
 var noneHaveTwoOrMoreVowels = function(words){
-  // Your Code Here
+  for (var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // #12
@@ -133,7 +168,12 @@ var noneHaveTwoOrMoreVowels = function(words){
 // e.g. given ['cat', 'horse', 'elephant'],
 //      return { cat: 3, horse: 5, elephant: 8}
 var buildObjectFromWords = function(words){
-  // Your Code Here
+  var result = {};
+  for (var i = 0; i < words.length; i++) {
+    result[words[i]] = words[i].length;
+  }
+  return result;
+};
 };
 
 
