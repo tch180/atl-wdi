ef4f620379d8bbb31531c565c21cb655c82e1340
tch180 <--global>
2017-09-24 09:58:59 -0400

    part 3: added new methods

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -13,17 +13,27 @@ class tamagotchi {
         this.foodinTummy--;
         console.log("WAAH!");
         console.log(this.name + ' has food in tummy = ' + this.foodinTummy);
-    };
+    }
+    puke(){
+        this.health --;
+        console.log("SICK AS A DOG")
+        console.log(this.name + ' has current health = ' + this.health);
+    }
+    yawn(){
+        this.restedness--;
+        console.log("I AM SO TIRED")
+        console.log(this.name + ' has current restedness of : ' + this.restedness);
+    }
 }
 
 //create new Tamagotchis
-var tamagotchi1 = new tamagotchi('jade', 'creatureType')
+var tamagotchi1 = new tamagotchi('jade', 'dog');
 var tamagotchi2 = new tamagotchi('elias', 'cat');
 
 console.log(tamagotchi1);
 console.log(tamagotchi2);
 //test out your Tamagotchies below via console.logs
-tamagotchi1.cry();
+tamagotchi1.yawn();
 tamagotchi2.cry();
 
 
