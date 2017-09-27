8d590da05e14a2fddef798a313e8dc8fa642711c
tch180 <--global>
2017-09-26 21:42:27 -0400

    created donut schema

--- homework/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,11 +1,21 @@
-/* //requirements: require mongoose
+ //requirements: require mongoose
 const mongoose = require('mongoose')
 
 //create your donut schema:
 const Schema = mongoose.Schema
 
-const DonutsSchema = new Schema ({
-
-})
- */
+const donutSchema = new Schema(
+    {
+           name: String,
+           description: String,
+           img: String,
+           price: Number,
+           qty: Number
+       })
+ const donutModel = mongoose.model('donut', donutSchema);
 //export your donut with module.exports()
+
+module.exports = donutModel
+
+
+
