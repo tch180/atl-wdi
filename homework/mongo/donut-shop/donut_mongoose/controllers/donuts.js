b6e163cc94a370c590d5ce3ea7f6404b635e65a9
tch180 <--global>
2017-09-27 20:04:39 -0400

    sample project

--- homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -7,60 +7,66 @@ const router = express.Router();
 const Schema = require("../models/donuts.js");
 //======================
 // INDEX
-//======================
+//====================== YES WORKING!!!!
 // Create a GET index route "/" that sends all donuts to index.hbs
 router.get('/', (request, response)=> {
     response.render('donuts/index')
+    console.log('hello index')
 })
 
 
 //======================
 // NEW
-//======================
+//====================== YES WORKING !!!!
 // Create a GET new route "/new" that renders the new.hbs form
 router.get('/new', (request, response) =>{
     response.render('donuts/new')
+    console.log('hello new')
 })
 
 
 //======================
 // SHOW
-//======================
+//====================== YES WORKING !!!!!
 // Create a GET show route "/:id" that renders the donut's show page
 router.get('/:id', (request, response)=>{
     response.render('donuts/show')
+    console.log('hello show')
 })
 
 
 
 //======================
 // CREATE
-//======================
+//======================  ******** 
 // Create a POST index route "/" that creates a new donut
 // and upon success redirects back to the index page "/"
 router.post('/', (request, response)=>{
-    response.redirect('/donuts')
+    response.redirect('/donuts/create')
+    console.log('hello create')
 })
 
 
 //======================
 // EDIT
-//======================
+//====================== YES WORKING !!!!
 // Create a GET edit route "/:id/edit" that renders the edit.hbs page and
 // sends that donut's data to it
 router.get('/:id/edit', (request, response)=> {
-    response.render('/edit'),{
+    response.render('donuts/edit'),{
+        
         
 }
 
 
 //======================
 // UPDATE
-//======================
+//====================== ******
 // Create a PUT update route "/:id" that updates the donut and
 // redirects back to the SHOW PAGE (not index)
 router.put('/:id', (request, response) => {
-    response.redirect('donuts/show')
+    response.redirect('/donuts/show')
+    console.log('hello update')
 })
 
 
