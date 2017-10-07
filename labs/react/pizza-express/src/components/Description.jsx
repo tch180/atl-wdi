9d4c27462934e62025b44db480b2beb24f71cb25
tch180 <--global>
2017-10-06 23:41:03 -0400

    homework update

--- labs/react/pizza-express/src/components/Description.jsx
+++ labs/react/pizza-express/src/components/Description.jsx
@@ -6,7 +6,7 @@ class Description extends Component {
         return(
             <div id="description"className="description">
                 <img src={PizzaOven} alt="pizza oven"/>
-                <p>Deep v jean shorts tumeric lo-fi. Pabst salvia hammock, neutra before they sold out fingerstache paleo viral flannel mlkshk kombucha echo park crucifix gochujang unicorn. Swag chia umami chillwave scenester. Scenester pop-up you probably haven't heard of them master cleanse. Poutine next level yr trust fund, meditation cloud bread flexitarian narwhal knausgaard schlitz four dollar toast offal put a bird on it lo-fi fanny pack. Literally skateboard migas mumblecore, celiac waistcoat health goth. Humblebrag cred neutra portland chillwave master cleanse keffiyeh copper mug enamel pin hexagon offal. Vape pop-up paleo cliche street art. Next level heirloom gluten-free viral cred. Crucifix ramps edison bulb, blue bottle lo-fi banh mi live-edge. Raw denim fingerstache celiac pug, squid succulents live-edge chillwave kale chips trust fund banjo. Viral air plant synth, beard tousled irony post-ironic chia crucifix iceland roof party </p>
+                <p>These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent. </p>
             </div>
         )
     }
